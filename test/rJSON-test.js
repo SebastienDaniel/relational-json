1a8940e86b05942a2279633658cb64366c090b84
SebastienDaniel <info@sebastiendaniel.ca>
2015-09-10 15:23:07 -0400

    added tests for extensions

--- test/rJSON-test.js
+++ test/rJSON-test.js
@@ -1,5 +1,6 @@
 var assert = require("chai").assert,
     should = require("chai").should(),
+    expect = require("chai").expect,
     rJSON = require("../src/scripts/rJSON.js");
 
 describe("rJSON", function() {
@@ -76,7 +77,75 @@ describe("rJSON", function() {
 
 
 // Extension
+describe("rJSON extension", function() {
+    var graph = {
+            Person: {
+                primary: "id",
+                fields: {
+                    person_id: {
+                        dataType: "integer",
+                        writable: false,
+                        allowNull: false
+                    },
+                    name: {
+                        dataType: "string",
+                        writable: true,
+                        allowNull: false
+                    },
+                    age: {
+                        dataType: "integer",
+                        writable: true,
+                        allowNull: false
+                    }
+                },
+                extends: {
+                    table: "People",
+                    local: "person_id",
+                    foreign: "id"
+                }
+            },
+            People: {
+                primary: "id",
+                fields: {
+                    id: {
+                        dataType: "integer",
+                        writable: false,
+                        allowNull: false
+                    },
+                    created_on: {
+                        dataType: "string",
+                        writable: true,
+                        allowNull: false
+                    }
+                }
+            }
+        },
+        db = rJSON(graph);
+
+    db.Person.post({person_id:1,name:"bob",age:30,created_on:"2015-01-01"});
+
+    it("should allow field inheritance", function() {
+        expect(db.Person.get(1).created_on).to.equal("2015-01-01");
+        expect(db.Person.get(1).id).to.equal(1);
+    });
+
+    it("should get PK from ancestor", function() {
+        expect(db.Person.get(1).id).to.equal(1);
+        expect(db.Person.get(1).person_id).to.equal(1);
+    });
+
+    it("fields should not be ownProperties", function() {
+        expect(Object.hasOwnProperty.call(db.Person.get(1),"name")).to.be.true;
+        expect(Object.hasOwnProperty.call(db.Person.get(1),"created_on")).not.to.be.true;
+        expect(Object.hasOwnProperty.call(db.Person.get(1),"id")).not.to.be.true;
+    });
+
+    it("ancestor fields should be modifiable from child face", function() {
+        db.Person.get(1).created_on = "2016-01-01";
+        expect(db.Person.get(1).created_on).to.equal("2016-01-01");
+        expect(db.People.get(1).created_on).to.equal("2016-01-01");
+    });
+});
 
 // Aggregation
 
-// Extension & aggregation
