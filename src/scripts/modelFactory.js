df87de0420289c7a7458e09b64b9f196b0baf0fd
Sebastien Daniel <info@sebastiendaniel.ca>
2016-04-17 15:14:23 -0400

    copy of required fields to avoid modification

--- src/scripts/modelFactory.js
+++ src/scripts/modelFactory.js
@@ -33,7 +33,7 @@ module.exports = function modelFactory(tn, fullModel) {
         },
         requiredFields: {
             get: function() {
-                return requiredFields;
+                return requiredFields.slice();
             }
         },
         aliasMap: {
