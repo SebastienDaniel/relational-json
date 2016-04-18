2fd3d35582da08d8247493e3869e40012153e903
Sebastien Daniel <info@sebastiendaniel.ca>
2016-04-18 07:21:28 -0400

    simplified required field compilation improved comments decoupled some code

--- src/scripts/model/getRequiredFields.js
+++ src/scripts/model/getRequiredFields.js
@@ -1,4 +1,13 @@
 /**
+ * tests if a field's metadata makes it a mandatory field
+ * @param {object} f - field metadata
+ * @returns {boolean}
+ */
+function fieldIsRequired(f) {
+    return !f.allowNull && !f.hasOwnProperty("defaultValue");
+}
+
+/**
  * compiles an array of all required field names to create a data object
  * for the provided table model
  * @param {object} model - table model to scan
@@ -7,21 +16,19 @@
  */
 module.exports = function getRequiredFields(model, fullModel) {
     "use strict";
-    var req = [];
 
     // get models' own required fields
-    Object.keys(model.fields).forEach(function(key) {
-        if (!model.fields[key].allowNull && !model.fields[key].hasOwnProperty("defaultValue")) {
-            req.push(key);
-        }
+    var req = Object.keys(model.fields).filter(function(key) {
+        return fieldIsRequired(model.fields[key]);
     });
 
-    // what about "extends" relations
+    // check for ancestor requirements
     if (model.extends) {
-        // recursively get required fields
+        // recursively get required fields of ancestor(s)
         req = req.concat(getRequiredFields(fullModel[model.extends.table], fullModel));
 
-        // remove the foreign extension field
+        // remove the ancestor extension field requirement,
+        // it can be inferred at data creation time
         req.splice(req.indexOf(model.extends.foreign), 1);
     }
 
