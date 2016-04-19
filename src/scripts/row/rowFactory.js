9233e5f8636df17ae742d21215dbff4516571992
Sebastien Daniel <info@sebastiendaniel.ca>
2016-04-19 11:52:22 -0400

    improved commenting removed redundant code improved own data encapsulation / pre-calculations

--- src/scripts/row/rowFactory.js
+++ src/scripts/row/rowFactory.js
@@ -1,5 +1,6 @@
 /**
- * Looks for an existing DB entry related to new row, based on ID
+ * Looks for an existing DB entry related to new row,
+ * based on ID and extends information
  * @param model
  * @param d
  * @param db
@@ -9,9 +10,7 @@ function getRowPrototype(model, d, db) {
 
     if (model.extends) {
         // try to fetch the parent
-        parent = db[model.extends.table].get(d[model.primary]);
-
-        if (parent) {
+        if (db[model.extends.table].get(d[model.primary])) {
             // update parent parent data
             // and use result as prototype (will be new obj or current parent)
             parent = db[model.extends.table].put(d, d[model.extends.local]);
@@ -29,7 +28,15 @@ function getRowPrototype(model, d, db) {
     }
 }
 
-// TODO: needs to encapsulate data
+/**
+ * compiles a row (object) based on its model & relations
+ * own data (model.fields) are hidden, and a public, read-only interface is created for that data.
+ * relations are dynamically mapped and data is requested on-demand.
+ * @param model
+ * @param d
+ * @param db
+ * @returns {Object}
+ */
 function rowFactory(model, d, db) {
     "use strict";
     var row = Object.create(getRowPrototype(model, d, db)),
@@ -38,44 +45,41 @@ function rowFactory(model, d, db) {
     // generate public row field descriptors
     Object.keys(model.fields).forEach(function(key) {
         if (model.extends && model.extends.local === key) {
-            // create field "getter" for extension points
-            // i.e. get parent data, don't duplicate data
-            Object.defineProperty(row, key, {
-                get: function() {
-                    return Object.getPrototypeOf(this)[model.extends.foreign];
-                },
-                enumerable: true
-            });
+            // create field "getter" for common parent-child data
+            // i.e. use parent data
+            data[key] = Object.getPrototypeOf(this)[model.extends.foreign];
         } else {
             // pre-calculate &  own data value
             data[key] = d[key] !== undefined ? d[key] : model.fields[key].defaultValue;
-
-            // add getter for own data
-            Object.defineProperty(row, key, {
-                get: function() {
-                    return data[key];
-                },
-                enumerable: true
-            });
         }
+
+        // add getter for own data (isolate)
+        Object.defineProperty(row, key, {
+            get: function() {
+                return data[key];
+            },
+            enumerable: true
+        });
     });
 
     // generate child extensions
+    // keep link dynamic (no pre-calculation)
     model.extendedBy.forEach(function(ext) {
-        // add getter for the parent data
+        // add getter for the child row
         Object.defineProperty(row, ext.foreignTable, {
             get: function() {
-                return db[ext.foreignTable].get(this[ext.localField]);
+                return db[ext.foreignTable].get(data[ext.localField]);
             },
             enumerable: true
         });
     });
 
     // add aggregates to row
+    // keep link dynamic (no pre-calculation)
     model.aggregates.forEach(function(agg) {
-        // add alias as property
+        // use alias as property name, fallback to foreign table name
         if (agg.cardinality === "many") {
-            Object.defineProperty(row, agg.alias, {
+            Object.defineProperty(row, agg.alias || agg.foreignTable, {
                 get: function() {
                     return db[agg.foreignTable].get().filter(function(d) {
                         return d[agg.foreignField] === this[agg.localField];
@@ -84,9 +88,9 @@ function rowFactory(model, d, db) {
                 enumerable: true
             });
         } else {
-            Object.defineProperty(row, agg.alias, {
+            Object.defineProperty(row, agg.alias || agg.foreignTable, {
                 get: function() {
-                    return db[agg.foreignTable].get(this[agg.localField]);
+                    return db[agg.foreignTable].get(data[agg.localField]);
                 },
                 enumerable: true
             });
