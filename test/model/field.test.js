040d2b11834cf4f1982d2a822e9cd4b7054b7e5c
Sebastien Daniel <info@sebastiendaniel.ca>
2016-04-20 20:42:40 -0400

    adapted tests to new structures

---
+++ test/model/field.test.js
@@ -0,0 +1,87 @@
+var expect = require("chai").expect,
+    Field = require("../../src/scripts/model/Field"),
+    schema = require("../data/mixed-graph");
+
+describe("Field", function() {
+    it("should create a Field instance", function() {
+        expect(new Field("id", schema["Entity"].fields.id)).to.be.instanceof(Field);
+    });
+    it("should contain props: \n\tname\n\tdataType\n\tallowNull", function() {
+        var f = new Field("created_on", schema["ExternalEntity"].fields.created_on);
+
+        expect(f).to.have.property("name");
+        expect(f.name).to.eql("created_on");
+
+        expect(f).to.have.property("dataType");
+        expect(f.dataType).to.eql("datetime");
+
+        expect(f).to.have.property("allowNull");
+        expect(f.allowNull).to.eql(false);
+    });
+    it("should set a default value of 'null' when allowNull === true, but no defaultValue is provided", function() {
+        var f = new Field("created_on", {
+                "allowNull": false,
+                "dataType": "datetime",
+                "writable": true
+            }),
+            f2 = new Field("created_on", {
+                "allowNull": true,
+                "dataType": "datetime",
+                "writable": true
+            });
+
+        expect(f.defaultValue).to.eql(undefined);
+        expect(f2.defaultValue).to.eql(null);
+    });
+
+    it("should have Field.prototype methods", function() {
+        var f = new Field("created_on", schema["ExternalEntity"].fields.created_on);
+
+        expect(f.isRequired).to.be.instanceof(Function);
+        expect(f.validateData).to.be.instanceof(Function);
+    });
+});
+
+describe("Field.isRequired()", function() {
+    it("should return true for required fields", function() {
+        var f = new Field("created_on", {
+                "allowNull": false,
+                "dataType": "datetime",
+                "writable": true
+            }),
+            f2 = new Field("created_on", {
+                "allowNull": true,
+                "dataType": "datetime",
+                "writable": true
+            }),
+            f3 = new Field("created_on", {
+                "allowNull": false,
+                "defaultValue": "hello",
+                "dataType": "datetime",
+                "writable": true
+            });
+
+        expect(f.isRequired()).to.be.true;
+    });
+    it("should return false for unrequired fields", function() {
+        var f = new Field("created_on", {
+                "allowNull": false,
+                "dataType": "datetime",
+                "writable": true
+            }),
+            f2 = new Field("created_on", {
+                "allowNull": true,
+                "dataType": "datetime",
+                "writable": true
+            }),
+            f3 = new Field("created_on", {
+                "allowNull": false,
+                "defaultValue": "hello",
+                "dataType": "datetime",
+                "writable": true
+            });
+
+        expect(f2.isRequired()).to.be.false;
+        expect(f3.isRequired()).to.be.false;
+    });
+});
--- test/model/getRequiredFields.test.js
+++ test/model/getRequiredFields.test.js
@@ -1,24 +1,43 @@
 var grf = require("../../src/scripts/model/getRequiredFields"),
     chai = require("chai"),
     expect = require("chai").expect,
-    compileModel = require("../../src/scripts/model/compileModel"),
-    graph = require("../data/test-graph.json"),
+    compileModel = require("../../src/scripts/model/buildModelGraph"),
+    graph = require("../data/mixed-graph.json"),
     model = compileModel(graph);
 
-describe("getRequiredFields", function() {
-    it("should return own required fields for a given Table", function() {
-        expect(grf("own", model["Entity"])).to.have.members(["id"]);
-        expect(grf("own", model["Entity"])).to.have.length(1);
+describe("getRequiredFields('own') & getRequiredFields()", function() {
+    it("should return own required fields, ignoring extends & aggregates", function() {
+        // using OWN
+        expect(grf(model["Entity"], "own")).to.have.members(["id"]);
+        expect(grf(model["Entity"], "own")).to.have.length(1);
+
+        expect(grf(model["ExternalEntity"], "own")).to.have.members(["entity_id", "created_by", "created_on"]);
+        expect(grf(model["ExternalEntity"], "own")).to.have.length(3);
+
+        expect(grf(model["Person"], "own")).to.have.members(["entity_id", "first_name", "last_name", "gender"]);
+        expect(grf(model["Person"], "own")).to.have.length(4);
+        
+        // no type argument
+        expect(grf(model["Entity"])).to.have.members(["id"]);
+        expect(grf(model["Entity"])).to.have.length(1);
+
+        expect(grf(model["ExternalEntity"])).to.have.members(["entity_id", "created_by", "created_on"]);
+        expect(grf(model["ExternalEntity"])).to.have.length(3);
+
+        expect(grf(model["Person"])).to.have.members(["entity_id", "first_name", "last_name", "gender"]);
+        expect(grf(model["Person"])).to.have.length(4);
     });
+});
 
-    it("should return own required fields \n\t& parent required fields\n\t& exclude extension fields of child", function() {
-        expect(grf("all", model.InternalEntity)).to.have.members(["created_on", "entity_id"]);
-        expect(grf("all", model.InternalEntity)).to.have.length(2);
+describe("getRequiredFields('all')", function() {
+    it("should return all required fields, including own, extends & aggregates", function() {
+        expect(grf(model["Entity"], "all")).to.have.members(["id"]);
+        expect(grf(model["Entity"], "all")).to.have.length(1);
 
-        expect(grf("all", model.ExternalEntity)).to.have.members(["created_on", "created_by", "entity_id"]);
-        expect(grf("all", model.ExternalEntity)).to.have.length(3);
+        expect(grf(model["ExternalEntity"], "all")).to.have.members(["entity_id", "created_by", "created_on"]);
+        expect(grf(model["ExternalEntity"], "all")).to.have.length(3);
 
-        expect(grf("all", model.Person)).to.have.members(["entity_id", "created_on", "created_by", "first_name", "last_name", "gender"]);
-        expect(grf("all", model.Person)).to.have.length(6);
+        expect(grf(model["Person"], "all")).to.have.members(["entity_id", "first_name", "last_name", "gender", "created_by", "created_on"]);
+        expect(grf(model["Person"], "all")).to.have.length(6);
     });
 });
