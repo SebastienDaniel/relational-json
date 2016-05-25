b49410f858f9e7f93efc4641a509ebaa0230d316
Sebastien Daniel <info@sebastiendaniel.ca>
2016-05-24 21:17:13 -0400

    added immutability test

--- test/immutability.test.js
+++ test/immutability.test.js
@@ -1,12 +1,26 @@
 var expect = require("chai").expect,
     schema = require("./data/mixed-graph.json"),
-    rjson = require("../src/scripts/index.js");
+    rjson = require("../src/scripts/index.js"),
+    dbMapper = function dbMapper(db) {
+        return Object.keys(db).reduce(function(obj, key) {
+            // get all table data
+            obj[key] = db[key].get().map(function(v) {
+                // show only own props for each table row
+                return Object.keys(v).reduce(function(o, k) {
+                    o[k] = v[k];
+
+                    return o;
+                }, {});
+            });
+
+            return obj;
+        }, {});
+    };
 
 describe("relational-json, immutability", function() {
-    var db = rjson(schema);
-    
     it("should provide immutability at the top level (data container)", function() {
-        var mem;
+        var db = rjson(schema),
+            mem;
 
         // create initial data
         db.Person.post({
@@ -35,5 +49,31 @@ describe("relational-json, immutability", function() {
 
         expect(db.Person.get(1).first_name).to.eql("bob");
         expect(mem[0].first_name).to.eql("seb");
+    });
+
+    it("should provide hierarchical immutability on PUT operations", function() {
+        var db = rjson(schema),
+            mem;
+
+        // create initial data
+        db.Person.post({
+            entity_id: 1,
+            first_name: "seb",
+            last_name: "dan",
+            gender: "m",
+            created_on: "2016-01-01T12:00:00Z",
+            created_by: 2
+        });
+
+        mem = db.Person.get(1);
+
+        // modify parent
+        //db.ExternalEntity.put({entity_id: 1, created_on: "2017-01-01T12:00:00Z"});
+
+        //console.log(dbMapper(db));
+        console.log(db.ExternalEntity.meta.aliasMap);
+
+        expect(db.Person.get()[0]).to.equal(mem);
+        expect(db.Person.get(1)).to.not.equal(mem);
     });
 });
