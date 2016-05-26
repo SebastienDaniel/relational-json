2e19c32711c5792fea766ac18ccf547e1bcc81e2
Sebastien Daniel <info@sebastiendaniel.ca>
2016-05-26 19:08:54 -0400

    re-integrated Dictionarys into Tables. My procedures should be using the Table interface for their operations

--- src/scripts/buildDatabase/index.js
+++
@@ -1,9 +0,0 @@
-var dictionary = require("./dictionaryFactory");
-
-function buildDatabase(dynamicModel) {
-    return Object.keys(dynamicModel).reduce(function(db, key) {
-        db[key] = dictionary();
-    }, Object.create(null));
-}
-
-module.exports = buildDatabase;
